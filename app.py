75d307fb70961edb16eecb0c2f5431557a14d16a
blackieghoster <blackieghoster@gmail.com>
2023-05-07 22:32:43 +0200

    added docstring

--- app.py
+++ app.py
@@ -5,7 +5,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>Hello WSB! Greetings from Flask!</h1>'
+    """
+    Index
+
+    :return: return index site code.
+    rtype: string
+
+    """
+    website = "<h1>Hello WSB! Greetings from Flask!</h1>"
+
+    return website
 
 
 if __name__ == "__main__":
